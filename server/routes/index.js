c61abd86daeabde60897d9ae56816ea092868ce9
vitorpinel <vitor.pinel@gmail.com>
2017-08-19 19:59:33 +1200

    MongoDb added to project

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var router = express.Router();
 
+
 import {getIndex} from "../handlers/indexHandler"
 
 // fires before entring the router continuation block
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -22,6 +22,7 @@ class Home extends React.Component{
         return(
                 <section id="page-title">
                     <div className="container clearfix">
+                    
                     </div>
                 </section>
         )
